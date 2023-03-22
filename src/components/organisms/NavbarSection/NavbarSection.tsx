0fa4b44683a9ca6e6d0ec86f7ec49d164926cfcd
eli castellano <eliman1926@gmail.com>
2023-03-22 15:13:10 -0400

    Cambiando size en icon

--- src/components/organisms/NavbarSection/NavbarSection.tsx
+++ src/components/organisms/NavbarSection/NavbarSection.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const NavbarSection = () => {
   return (
     <div className='flex'>
-      <img src="/assets/transparent.png" className='mt-4' alt="" />
+      <img src="/assets/transparent.png" className='h-[57px] w-[54px] sm:h-auto sm:w-auto mt-4' alt="" />
       <TranslateBtn styles='ml-auto' />
     </div>
   )
